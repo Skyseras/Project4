65d8e29c40576a20bff9523ee7794293b0fc00de
Skyseras <yasserelmahfoudi@gmail.com>
2022-10-12 14:56:05 +0100

    done

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '41i0jwq1q4';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-kyqh0f8b.us.auth0.com',
+  clientId: 'YtgR5XdXDVtaz7dkjAIE7MGsGgxd7LKI',
+  callbackUrl: 'http://localhost:3000/callback'
+};
